7afa20b73f13c1b25b725086775cb3742f84ae05
maks <maks@coderiver.com.ua>
2018-10-19 14:39:46 +0300

    saving favorite films in local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,30 @@
+
 export const favoriteList = {
   list: [],
   addFilm (film) {
     this.list.push(film)
-    // localStorage.setItem('auth', JSON.stringify({
-    //   isAuthenticated: this.isAuthenticated
-    // }));
+    localStorage.setItem('filmsList', JSON.stringify({
+      list: this.list
+    }))
   },
   removeFilm (filmId) {
     this.list = this.list.filter(item => {
       return filmId !== item.id
     })
-  // localStorage.setItem('auth', JSON.stringify({
-  //   isAuthenticated: this.isAuthenticated
-  // }));
+    localStorage.setItem('filmsList', JSON.stringify({
+      list: this.list
+    }))
   }
+}
+
+let storedFilms = JSON.parse(localStorage.getItem('filmsList'))
+console.log(storedFilms)
+if (storedFilms) {
+  favoriteList.list = storedFilms.list
+} else {
+  localStorage.setItem('filmsList', JSON.stringify({
+    list: this.list
+  }))
 }
 
 export const apiKey = '5fc1fc09c4439e9e384f1dce452ba65c'
