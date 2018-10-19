46b45a35625fa544d1f9555240c35313a5c70626
maks <maks@coderiver.com.ua>
2018-10-19 18:11:01 +0300

    add deploy to gh pages

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ npm run build
 cd dist
 cp index.html 404.html
 git init
-git remote add origin
+git remote add origin https://github.com/nikomax/nikomax.github.io.git
 git add -A
 git commit -m "Build"
 git push -f origin master
